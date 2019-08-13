4bb07316749809113017901057f819ffa242b3c3
Janik313 <54064741+Janik313@users.noreply.github.com>
2019-08-13 19:39:06 +0200

    Update [recaptcha skip].js

--- [recaptcha skip].js
+++ [recaptcha skip].js
@@ -1,23 +1,23 @@
 // ==UserScript==
-// @name         [Captcha Skip]
+// @name         [reCaptcha Skip]
 // @version      1.0.0
-// @description  This solve google recaptcha automatic after sec.
-// @author       Janik313
-// @match	 ebonus.gg google.com
+// @description  This solve google recaptcha automatic after 2.5 sec.
+// @author       Janik313 [github.com/Janik313]
+// @match	     *
 // @include      *
 // @grant        none
-// @namespace https://greasyfork.org/users/200015
+// @namespace    https://github.com/Janik313/recaptchaSkip/blob/master/%5Brecaptcha%20skip%5D.js
 // ==/UserScript==
 
 var domain = (window.location != window.parent.location) ? document.referrer.toString() : document.location.toString();
-if (domain.indexOf('miped.ru') == -1 && domain.indexOf('indiegala') == -1 && domain.indexOf('gleam.io') == -1) { //You can exclude domains here (advanced)
+if (domain.indexOf('*') == -1 && domain.indexOf('*') == -1 && domain.indexOf('*') == -1) { //You can exclude domains here (advanced)
     if (location.href.indexOf('google.com/recaptcha') > -1) {
         var clickCheck = setInterval(function() {
             if (document.querySelectorAll('.recaptcha-checkbox-checkmark').length > 0) {
                 clearInterval(clickCheck);
                 document.querySelector('.recaptcha-checkbox-checkmark').click();
             }
-        }, 5000);
+        }, 2500);
     } else {
         var forms = document.forms;
         for (var i = 0; i < forms.length; i++) {
