088a51adfd3405b127916dfd9e90c2b9e89b76a1
Janik313 <54064741+Janik313@users.noreply.github.com>
2019-08-13 19:30:34 +0200

    Add files via upload

---
+++ [recaptcha skip].js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         [Captcha Skip]
+// @version      1.0.0
+// @description  This solve google recaptcha automatic after sec.
+// @author       Janik313
+// @match	 ebonus.gg google.com
+// @include      *
+// @grant        none
+// @namespace https://greasyfork.org/users/200015
+// ==/UserScript==
+
+var domain = (window.location != window.parent.location) ? document.referrer.toString() : document.location.toString();
+if (domain.indexOf('miped.ru') == -1 && domain.indexOf('indiegala') == -1 && domain.indexOf('gleam.io') == -1) { //You can exclude domains here (advanced)
+    if (location.href.indexOf('google.com/recaptcha') > -1) {
+        var clickCheck = setInterval(function() {
+            if (document.querySelectorAll('.recaptcha-checkbox-checkmark').length > 0) {
+                clearInterval(clickCheck);
+                document.querySelector('.recaptcha-checkbox-checkmark').click();
+            }
+        }, 5000);
+    } else {
+        var forms = document.forms;
+        for (var i = 0; i < forms.length; i++) {
+            if (forms[i].innerHTML.indexOf('google.com/recaptcha') > -1) {
+                var rc_form = forms[i];
+                var solveCheck = setInterval(function() {
+                    if (grecaptcha.getResponse().length > 0) {
+                        clearInterval(solveCheck);
+                        rc_form.submit();
+                    }
+                }, 100);
+            }
+        }
+    }
+}
